aa0b991c84821800412562bdeca2df96154941e5
benedikt <benedikt@xenzilla.de>
2012-07-13 01:16:44 +0200

    Reset
    Change-Id: Ie24409ab68e539f8e77a6b8134229a2ec9404787

--- application/controllers/license.php
+++
@@ -1,8 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-class License extends CI_Controller {
-
-    function index() {
-        $this->load->view('license');         
-    }
-}
