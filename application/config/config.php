dc9500e02687632e3a1011b90a02e2e012e2a224
Benedikt Bauer <benedikt@xenzilla.de>
2016-08-22 22:56:04 +0200

    CI is a bit sensitive to config options casing
    Apparently all options must be uppercase, EXCEPT for the charset option

--- application/config/config.php
+++ application/config/config.php
@@ -80,7 +80,7 @@ $config['language']	= 'english';
 | that require a character set to be provided.
 |
 */
-$config['charset'] = 'UTF-8';
+$config['charset'] = 'utf-8';
 
 /*
 |--------------------------------------------------------------------------
