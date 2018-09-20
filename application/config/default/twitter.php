0d283f3170c425e8eb7696a092f5d132f27bc83c
Benedikt Bauer <benedikt@xenzilla.de>
2018-09-20 21:39:17 +0200

    Fix tabs vs spaces

--- application/config/default/twitter.php
+++ application/config/default/twitter.php
@@ -2,11 +2,11 @@
 
     $config['twitter_consumer_key'] = '';
     $config['twitter_consumer_secret'] = '';
-    $config['twitter_api_url'] = 'https://api.twitter.com/';
-    $config['twitter_streamapi_url'] = 'https://stream.twitter.com/';
-    $config['twitter_request_url'] = 'oauth/request_token';
-    $config['twitter_authorize_url'] = 'oauth/authorize';
-    $config['twitter_access_url'] = 'oauth/access_token';
-
+    $config['twitter_api_url'] = 'https://api.twitter.com/1.1/';
+    $config['twitter_request_url'] = 'https://api.twitter.com/oauth/request_token';
+    $config['twitter_authorize_url'] = 'https://api.twitter.com/oauth/authorize';
+    $config['twitter_access_url'] = 'https://api.twitter.com/oauth/access_token';
+    
+    
 /* End of file twitter.php */
 /* Location: ./application/config/twitter.php */
