c062f47dbd5d28537f61009a4a1313a57f4b8d24
Benedikt Bauer <benedikt@xenzilla.de>
2018-09-29 19:17:34 +0200

    CAPITALIZE Env vars

--- application/config/production/twitter.php
+++ application/config/production/twitter.php
@@ -1,7 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-    $config['twitter_consumer_key'] = $_ENV['twitter_api_key'];;
-    $config['twitter_consumer_secret'] = $_ENV['twitter_api_secret'];
+    $config['twitter_consumer_key'] = $_ENV['TWITTER_API_KEY'];;
+    $config['twitter_consumer_secret'] = $_ENV['TWITTER_API_SECRET'];
     $config['twitter_api_url'] = 'https://api.twitter.com/1.1/';
     $config['twitter_streamapi_url'] = 'https://stream.twitter.com/';
     $config['twitter_request_url'] = 'https://api.twitter.com/oauth/request_token';
