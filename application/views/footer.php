e3dac39eb5dd0efaae9a957b6c0a45e010046a32
Benedikt Bauer <benedikt@xenzilla.de>
2020-05-15 23:04:51 +0200

    Change footer, remove hints on paid version, remove date, update imprint with new address

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,10 +1,10 @@
         <div class="section">
             <img src="<?php echo $base?>images/paypal_64.png" />
-            <h2>Donations and Paid Features</h2>
+            <h2>Donations</h2>
             <p>
                 This service is provided for free on an &ldquo;as is&rdquo; basis as described above.<br />
                 If you like this service though, please consider donating a small amount via PayPal.<br />
-                The sourcecode is available for free under the <a class="hiddenlink" href="<?php echo $base?>license">do what the fuck you want to public license v2</a><br />
+                The sourcecode is available for free under the &ldquo;<a class="hiddenlink" href="<?php echo $base?>license">do what the fuck you want to public license v2</a>&rdquo;<br />
                 It can be downloaded at the <a class="hiddenlink" href="https://github.com/mastacheata/song-poster">public GitHub repository</a>, though no assistance or support of any kind is given.
             </p>
             <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
@@ -24,13 +24,11 @@
 
         <div class="credits">
             <p>
-                The copyright for all content within this website including the facebook and twitter application is held by Benedikt Bauer - Kreuzherrenstra&szlig;e 8 - 52062 Aachen - Germany.<br />
+                The copyright for all content within this website including the facebook and twitter application is held by Benedikt Bauer - Oktavianstraße 19 - 50968 Köln - Germany.<br />
                 Copyright for the SAM Logo in the Twitter and Facebook Directories is held by <a href="https://store.spacial.com/545/cookie?affiliate=27245&redirectto=http%3a%2f%2fspacial.com%2f" rel="external" class="hiddenlink" target="_blank">SpacialAudio Solutions LLC</a> and used with kind permission.<br />
                 The developer of this application is not affiliated with Spacial Audio Solutions LLC and the applications are neither developed by nor officially supported by them.
             </p>
         </div>
-
-        <div style="font-size: 8pt;">01.09.2012 - 01:15</div>
         <script type="text/javascript">
             var uvOptions = {};
             (function() {
